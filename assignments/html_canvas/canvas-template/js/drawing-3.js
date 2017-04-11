e7583c29d6ed649867bd2fec28ab035361d55d58
ryantc94 <rtc270@nyu.edu>
2017-04-11 00:59:26 -0400

    Pic Fix

--- assignments/html_canvas/canvas-template/js/drawing-3.js
+++ assignments/html_canvas/canvas-template/js/drawing-3.js
@@ -2,7 +2,9 @@
 
 function setup() {
   createCanvas(600, 450);
-  var doggoPhoto = loadImage('../media/doggo.png')
+
+  //global
+  doggoPhoto = loadImage('../media/doggo.png')
 }
 
 function draw() {
