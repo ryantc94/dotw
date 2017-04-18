c1a974b6f91f6836be65eaaeda4723a4da73ed67
ryantc94 <rtc270@nyu.edu>
2017-04-18 13:26:06 -0400

    pixel

--- assignments/html_canvas/canvas-template/js/drawing-3.js
+++ assignments/html_canvas/canvas-template/js/drawing-3.js
@@ -1,21 +1,12 @@
-var canvas = document.getElementById('drawing');
-var context = canvas.getContext('2d');
+// Drawing 3
 
-var dog = new Image();
-dog.src = 'media/doggo.jpeg';
+function setup() {
+  createCanvas(600, 450);
+
+  //global
+  doggoPhoto = loadImage('media/doggo.jpeg');
+}
 
 function draw() {
-  context.drawImage(dog, 0, 0);
+	image(doggoPhoto, 0, 0);
 }
-
-var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-var data = imageData.data;
-
-for (var i = 0; i < data.length; i += 4) {
-	data[i] = 255 - data[i];
-	data[i + 1] = 255 - data[i + 1];
-	data[i + 2] = 255 - data[i + 2]; 
-}
-
-// redraw image with updated pixels
-context.putImageData(imageData, 0, 0);
