b10a48a27f1f166d241217816e6e96869f0e0ed5
ryantc94 <rtc270@nyu.edu>
2017-04-18 08:21:45 -0500

    wow

--- assignments/html_canvas/canvas-template/js/drawing-1.js
+++ assignments/html_canvas/canvas-template/js/drawing-1.js
@@ -18,7 +18,7 @@ function draw() {
 	video.loadPixels();
 	// image(dogAttack, 0, 0, 800, 450);
 
-	ellipseMode(center);
+	ellipseMode(CENTER);
 
 	for (var y = 0; y < video.height; y++) { 
     	for (var x = 0; x < video.width; x++) { 
@@ -27,7 +27,7 @@ function draw() {
       		// video.pixels[index] = random(255);
       		// var g = video.pixels[index + 1]; 
       		// var b = video.pixels[index + 2];
-      		
+
       		fill(random(255), random(255), random(255));
       		noStroke();
       		ellipse(x * vScale, y * vScale, vScale-1, vScale-1);
