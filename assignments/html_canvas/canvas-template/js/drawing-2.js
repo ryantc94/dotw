ac6012d30c3131e521bd4922a554b69ca169042d
ryantc94 <rtc270@nyu.edu>
2017-04-18 00:51:44 -0500

    circles

--- assignments/html_canvas/canvas-template/js/drawing-2.js
+++ assignments/html_canvas/canvas-template/js/drawing-2.js
@@ -18,11 +18,11 @@ function draw() {
 	ellipseMode(CENTER);
 
 	ellipse(300, 75, 50, 50);
-	ellipse(410, 120, 50, 50);
+	ellipse(400, 120, 50, 50);
 	ellipse(450, 225, 50, 50);
-	ellipse(410, 330, 50, 50);
+	ellipse(400, 330, 50, 50);
 	ellipse(300, 375, 50, 50);
-	// ellipse(300, 75, 50, 50);
+	ellipse(200, 330, 50, 50);
 	// ellipse(300, 75, 50, 50);
 	// ellipse(300, 75, 50, 50);
 
