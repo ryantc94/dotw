08f39e11ac0bc2aa9cf6019b05e025de59c65ba8
ryantc94 <rtc270@nyu.edu>
2017-04-18 13:09:10 -0400

    pixel

--- assignments/html_canvas/canvas-template/js/drawing-3.js
+++ assignments/html_canvas/canvas-template/js/drawing-3.js
@@ -1,6 +1,12 @@
 	// Drawing 3
 var picture; 
 var vScale = 10;
+var c1 = random(255);
+var c2 = random(255);
+var c3 = random(255);
+
+var x = [];
+
 
 function preload(){
 	picture = loadImage('media/doggo.jpeg');
@@ -11,26 +17,22 @@ function setup() {
 
   	picture.loadPixels();
 
-	ellipseMode(CORNER);
-
-	for (var y = 0; y < picture.height; y++) { 
-    	for (var x = 0; x < picture.width; x++) { 
-      		var index = (x + y * picture.width) * 4; 
-
-      		var r = picture.pixels[index]; // access red value of current pixel
-      		var g = picture.pixels[index + 1]; // access green value
-      		var b = picture.pixels[index + 2]; // access blue value
-
-      		var gray = (r + g + b) / 3;
-
-      		fill(gray);
-      		noStroke();
-      		ellipse(x * vScale, y * vScale, vScale-1, vScale-1);
-      	}
-    }	
+	
 }
 
 function draw() {
 
-	//sound https://www.youtube.com/watch?v=NF1lwZ24RYI
+	ellipseMode(CORNER);
+
+	if (circleHover == true) {
+    	for (var y = 0; y < picture.height; y++) { 
+    		for (var x = 0; x < picture.width; x++) { 
+      			var index = (x + y * picture.width) * 4; 
+      			
+      			fill(c1, c2, c3);
+	      		noStroke();
+	      		ellipse(x * vScale, y * vScale, vScale-1, vScale-1);
+      		}
+  		}
+  	}		
 }
