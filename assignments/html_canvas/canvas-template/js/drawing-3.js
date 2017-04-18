a59d5f3487632561f8dd1362cce55a3f1935a808
ryantc94 <rtc270@nyu.edu>
2017-04-18 12:52:29 -0400

    wow

--- assignments/html_canvas/canvas-template/js/drawing-3.js
+++ assignments/html_canvas/canvas-template/js/drawing-3.js
@@ -1,14 +1,32 @@
 	// Drawing 3
+var picture; 
+var vScale = 10;
+
+function preload(){
+	picture = loadImage('media/doggo.jpeg');
+}
 
 function setup() {
   createCanvas(600, 450);
 
-  //global
-  doggoPhoto = loadImage('media/doggo.jpeg');
 }
 
 function draw() {
 
 	//sound https://www.youtube.com/watch?v=NF1lwZ24RYI
-	image(doggoPhoto, 0, 0);
+
+	picture.loadPixels();
+
+	ellipseMode(CORNER);
+
+	for (var y = 0; y < picture.height; y++) { 
+    	for (var x = 0; x < picture.width; x++) { 
+      		var index = (x + y * picture.width) * 4; 
+
+
+      		fill(random(255), random(255), random(255))
+      		noStroke();
+      		ellipse(x * vScale, y * vScale, vScale-1, vScale-1);
+      	}
+    }	
 }
