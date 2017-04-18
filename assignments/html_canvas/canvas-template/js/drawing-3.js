0fcb4de484100c89745a6d20fafeb51f66725c9b
ryantc94 <rtc270@nyu.edu>
2017-04-18 13:11:58 -0400

    pixel

--- assignments/html_canvas/canvas-template/js/drawing-3.js
+++ assignments/html_canvas/canvas-template/js/drawing-3.js
@@ -4,6 +4,7 @@ var vScale = 10;
 var c1 = random(255);
 var c2 = random(255);
 var c3 = random(255);
+var circleHover;
 
 var x = [];
 
@@ -24,15 +25,29 @@ function draw() {
 
 	ellipseMode(CORNER);
 
+	var distance = dist(mouseX, mouseY, width/2, height/2);
+
+  // Determine if distance is less than circle radius
+	if (distance < 50) {
+		circleHover = true;
+	} else {
+	    circleHover = false;
+	}
+
+	ellipseMode(CENTER);
+  	noStroke();
+
 	if (circleHover == true) {
     	for (var y = 0; y < picture.height; y++) { 
     		for (var x = 0; x < picture.width; x++) { 
       			var index = (x + y * picture.width) * 4; 
-      			
+
       			fill(c1, c2, c3);
 	      		noStroke();
 	      		ellipse(x * vScale, y * vScale, vScale-1, vScale-1);
       		}
   		}
-  	}		
+  	}	
+  		
+  	ellipse(width/2, height/2, 100, 100);
 }
