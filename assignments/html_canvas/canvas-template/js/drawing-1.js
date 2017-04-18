891ff140c9ce37d1ef90d61e68dab17d3d0f00bf
ryantc94 <rtc270@nyu.edu>
2017-04-18 12:53:45 -0400

    new

--- assignments/html_canvas/canvas-template/js/drawing-1.js
+++ assignments/html_canvas/canvas-template/js/drawing-1.js
@@ -13,24 +13,3 @@ function setup() {
   video.loop();
   video.hide();
 }
-
-function draw() {
-	video.loadPixels();
-	// image(dogAttack, 0, 0, 800, 450);
-
-	ellipseMode(CENTER);
-
-	for (var y = 0; y < video.height; y++) { 
-    	for (var x = 0; x < video.width; x++) { 
-      		var index = (x + y * video.width) * 4; 
-
-      		var r = video.pixels[index]; // access red value of current pixel
-      		var g = video.pixels[index + 1]; // access green value
-      		var b = video.pixels[index + 2]; // access blue value
-
-      		var gray = (r + g + b) / 2; 
-      		noStroke();
-      		ellipse(x * vScale, y * vScale, vScale-1, vScale-1);
-      	}
-    }	
-}
