0653df95417f5a7687e441d7f2642c122cc0d746
ryantc94 <rtc270@nyu.edu>
2017-04-20 01:22:24 -0400

    change

--- assignments/html_canvas/canvas-template/js/drawing-3.js
+++ assignments/html_canvas/canvas-template/js/drawing-3.js
@@ -1,17 +1,14 @@
 // Drawing 3
 
-function preload() {
-	doggoPhoto = loadImage('media/doggo.jpeg');
-}
-
 function setup() {
 	createCanvas(600, 450);
 
+	doggoPhoto = loadImage('media/doggo.jpeg');
 	loadPixels();
 	pxd = pixelDensity();
 	pxWidth = width * pxd;
   	pxHeight = height * pxd;
-  	
+
 	for (var y = 0; y < pxHeight; y++) { // for every row
     	for (var x = 0; x < pxWidth; x++) { // visit every column (pixel)
       		var index = (x + y * pxWidth) * 4; // index position of every pixel
@@ -23,13 +20,13 @@ function setup() {
     	}
   	}
   	updatePixels();
+  	blendMode(EXCLUSION);
+  	image(doggoPhoto, 0, 0);
 
   //global
 	doggoPhotoMouse = loadImage('media/doggo.jpeg');
 }
 
 function draw() {
-	image(doggoPhoto, 0, 0);
-
 	image(doggoPhotoMouse, mouseX, mouseY, 100, 100);
 }
