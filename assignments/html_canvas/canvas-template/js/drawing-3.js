1e02b9c076f25ece6e302392b2394596ebbb71a9
ryantc94 <rtc270@nyu.edu>
2017-04-20 01:20:29 -0400

    change

--- assignments/html_canvas/canvas-template/js/drawing-3.js
+++ assignments/html_canvas/canvas-template/js/drawing-3.js
@@ -8,6 +8,10 @@ function setup() {
 	createCanvas(600, 450);
 
 	loadPixels();
+	pxd = pixelDensity();
+	pxWidth = width * pxd;
+  	pxHeight = height * pxd;
+  	
 	for (var y = 0; y < pxHeight; y++) { // for every row
     	for (var x = 0; x < pxWidth; x++) { // visit every column (pixel)
       		var index = (x + y * pxWidth) * 4; // index position of every pixel
