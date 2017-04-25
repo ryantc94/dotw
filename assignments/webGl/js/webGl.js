65a777bd2026be106259a79129103f2ea606a530
ryantc94 <rtc270@nyu.edu>
2017-04-25 13:18:21 -0400

    iamges

--- assignments/webGl/js/webGl.js
+++ assignments/webGl/js/webGl.js
@@ -1,67 +1,56 @@
-
-var camera, scene, renderer, controls, spotlight;
+var myCamera, scene, light, renderer, controls;
 var geometry, material, mesh;
 
 var container = document.getElementById('container');
 
 function init() {
-	scene = new THREE.Scene();
-	
-	var path = "media/mp_orbital/";
-  	var format = ".jpg";
-  	var urls = [
-    	path + 'pos-x' + format, path + 'neg-x' + format,
-    	path + 'pos-y' + format, path + 'neg-y' + format,
-    	path + 'pos-z' + format, path + 'neg-z' + format
-  	];
+  scene = new THREE.Scene();
 
-  	scene.background = new THREE.CubeTextureLoader().load(urls);
+  // skybox files
+  var path = "media/mp_orbital/";
+  var format = ".jpg";
+  var urls = [
+    path + 'pos-x' + format, path + 'neg-x' + format,
+    path + 'pos-y' + format, path + 'neg-y' + format,
+    path + 'pos-z' + format, path + 'neg-z' + format
+  ];
 
-  	var width = window.innerWidth;
-	var height = window.innerHeight;
+  scene.background = new THREE.CubeTextureLoader().load(urls);
 
-	camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 25000);
-	camera.position.set(0, 200, 700);
-	scene.add(camera);
+  var width = window.innerWidth;
+  var height = window.innerHeight;
 
-	var light = new THREE.DirectionalLight(0xffffff, 1);
-	light.position.set(1, 1, 1); // location x, y, z
-	scene.add(light);
+  myCamera = new THREE.PerspectiveCamera(45, width/height, 1, 25000);
+  myCamera.position.set(0, 0, 600);
+  scene.add(myCamera);
 
-	// spotlight = new THREE.SpotLight(0xffffff, 0.8, 2000); // color, intensity, distance
-	// spotlight.position.set(500, 500, 500);
-	// spotlight.castShadow = true;
-	// scene.add(spotlight);
+  light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(1, 1, 1);
+  scene.add(light);
 
-	// spotlight.shadow.mapSize.width = 4096;
-	// spotlight.shadow.mapSize.height = 4096;
-	// spotlight.shadow.camera.near = 500;
-	// spotlight.shadow.camera.far = 2000;
-	// spotlight.shadow.camera.fov = 45;
+  material = new THREE.MeshBasicMaterial({envMap: scene.background});
+  geometry = new THREE.SphereGeometry(100, 50, 50);
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-	geometry = new THREE.SphereGeometry(100, 20, 20); // radius, additional vertices x and y
-	material = new  THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true});
-	mesh = new THREE.Mesh(geometry, material); // combine geometry and material
-	mesh.position.y = 120;
-	scene.add(mesh);
+  renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+  renderer.setSize(width, height);
 
-	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10); // x, y, vertices
-	var floorMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
-	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
-	floor.rotation.x = Math.PI / 2; // rotate to lay flat
-	scene.add(floor);
-
-	renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
-	renderer.setSize(width, height);
-	controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-	container.appendChild(renderer.domElement);
+  container.appendChild(renderer.domElement);
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  controls.update();
+
+  // Animate camera around sphere
+  var date = new Date();
+  var timer = date.getTime() * 0.0002;
+  myCamera.position.x = 600 * Math.cos(timer); // 600 is z position of camera
+  myCamera.position.z = 600 * Math.sin(timer);
+  myCamera.lookAt(scene.position);
+  myCamera.updateMatrixWorld();
+
+  renderer.render(scene, myCamera);
 }
 
 init();
