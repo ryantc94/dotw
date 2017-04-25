094b7b98e9600d57fcc87324255aceac8a6d508e
ryantc94 <rtc270@nyu.edu>
2017-04-25 02:02:20 -0400

    ?

--- assignments/webGl/js/webGl.js
+++ assignments/webGl/js/webGl.js
@@ -11,25 +11,31 @@ function init() {
 	camera.position.set(0, 200, 700);
 	scene.add(camera);
 
-	// var light = new THREE.DirectionalLight(0xffffff, 1);
-	// light.position.set(1, 1, 1); // location x, y, z
-	// scene.add(light);
+	var light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(1, 1, 1); // location x, y, z
+	scene.add(light);
 
-	// spotlight = new THREE.SpotLight(0xffffff, 0.8, 2000); // color, intensity, distance
-	// spotlight.position.set(500, 500, 500);
-	// spotlight.castShadow = true;
-	// scene.add(spotlight);
+	spotlight = new THREE.SpotLight(0xffffff, 0.8, 2000); // color, intensity, distance
+	spotlight.position.set(500, 500, 500);
+	spotlight.castShadow = true;
+	scene.add(spotlight);
 
-	// spotlight.shadow.mapSize.width = 4096;
-	// spotlight.shadow.mapSize.height = 4096;
-	// spotlight.shadow.camera.near = 500;
-	// spotlight.shadow.camera.far = 2000;
-	// spotlight.shadow.camera.fov = 45;
+	spotlight.shadow.mapSize.width = 4096;
+	spotlight.shadow.mapSize.height = 4096;
+	spotlight.shadow.camera.near = 500;
+	spotlight.shadow.camera.far = 2000;
+	spotlight.shadow.camera.fov = 45;
 
 	geometry = new THREE.OctahedronGeometry(200, 0); // radius, additional vertices
 	material = new  THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true});
 	mesh = new THREE.Mesh(geometry, material); // combine geometry and material
 	scene.add(mesh);
+
+	var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10); // x, y, vertices
+	var floorMaterial = new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true});
+	var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+	floor.rotation.x = Math.PI / 2; // rotate to lay flat
+	scene.add(floor);
 
 	renderer = new THREE.WebGLRenderer({alpha: 1, antialias: true});
 	renderer.setSize(width, height);
