cf288c34b4e387a84411557ee500e0a08487181f
ryantc94 <rtc270@nyu.edu>
2017-04-25 11:43:59 -0400

    iamges

--- assignments/webGl/js/webGl.js
+++ assignments/webGl/js/webGl.js
@@ -6,7 +6,7 @@ function init() {
 	scene = new THREE.Scene();
 	
 	var path = "media/mp_orbital/";
-  	var format = ".tga";
+  	var format = ".png";
   	var urls = [
     	path + 'pos-x' + format, path + 'neg-x' + format,
     	path + 'pos-y' + format, path + 'neg-y' + format,
