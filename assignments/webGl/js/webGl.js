97ab41eb9e5be8bfe89182800ac26c8bf881a5ac
ryantc94 <rtc270@nyu.edu>
2017-04-30 14:41:28 -0400

    cream

--- assignments/webGl/js/webGl.js
+++ assignments/webGl/js/webGl.js
@@ -32,12 +32,19 @@ function init() {
 
   var loader = new THREE.BufferGeometryLoader();
 
-  loader.load('media/fireMonkey.json', function(modelGeometry) {
+  loader.load('media/icecream.json', function(modelGeometry) {
     var modelMesh = new THREE.Mesh(modelGeometry, modelMaterial);
     modelMesh.scale.set(40, 40, 40);
     scene.add(modelMesh);
   });
-  
+ 
+  var planeMaterial = new THREE.MeshStandardMaterial({color: 0xffffff, side: THREE.DoubleSide});
+  var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+  plane.rotation.x = Math.PI / -2;
+  plane.receiveShadow = true;
+  scene.add(plane);
+
   renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
   renderer.setSize(width, height);
 
