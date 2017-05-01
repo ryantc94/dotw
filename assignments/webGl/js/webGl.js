65270d1914e808a21dc83d191403970c11c520d3
ryantc94 <rtc270@nyu.edu>
2017-05-01 14:21:02 -0400

    iceCream

--- assignments/webGl/js/webGl.js
+++ assignments/webGl/js/webGl.js
@@ -1,78 +1,69 @@
-var camera, scene, renderer, controls, spotlight;
+var myCamera, scene, light, renderer, controls;
 var geometry, material, mesh;
+
+var container = document.getElementById('symbol');
 
 function init() {
   scene = new THREE.Scene();
+
+  // // skybox files
+  // var path = "images/glacier/";
+  // var format = ".jpg";
+  // var urls = [
+  //   path + 'pos-x' + format, path + 'neg-x' + format,
+  //   path + 'pos-y' + format, path + 'neg-y' + format,
+  //   path + 'pos-z' + format, path + 'neg-z' + format
+  // ];
+
+  // scene.background = new THREE.CubeTextureLoader().load(urls);
+
   var width = window.innerWidth;
   var height = window.innerHeight;
 
-  camera = new THREE.PerspectiveCamera(45, width/height, 0.1, 25000);
-  camera.position.set(0, 200, 700);
-  scene.add(camera);
+  myCamera = new THREE.PerspectiveCamera(45, width/height, 100, 25000);
+  myCamera.position.set(0, 300, 800);
+  scene.add(myCamera);
 
-  var light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(1, 1, 1); // location x, y, z
+  light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(1, 1, 1);
   scene.add(light);
 
-  spotlight = new THREE.SpotLight(0xffffff, 0.8, 2000); // color, intensity, distance
-  spotlight.position.set(500, 500, 500);
-  spotlight.castShadow = true;
-  scene.add(spotlight);
-
-  // shadow map texture width
-  spotlight.shadow.mapSize.width = 4096;
-  // shadow map texture height
-  spotlight.shadow.mapSize.height = 4096;
-
-  // perspective shadow camera frustum near parameter
-  spotlight.shadow.camera.near = 500;
-  // perspective shadow camera frustum far parameter
-  spotlight.shadow.camera.far = 2000;
-  // perspective shadow camera frustum field of view
-  spotlight.shadow.camera.fov = 45;
-
-  // view shadow camera
-  // var helper = new THREE.CameraHelper(spotlight.shadow.camera);
-  // scene.add(helper);
-
-  var modelMaterial = new THREE.MeshStandardMaterial({color: 0xffffff})
+  material = new THREE.MeshStandardMaterial({envMap: scene.background, side: THREE.DoubleSide});
 
   var loader = new THREE.BufferGeometryLoader();
 
-
-  //http://stackoverflow.com/questions/28203846/three-js-blender-is-only-exporting-a-single-object
-  //if this works you have to click geometry and click faceMaterials then switch back to BufferedGeometry
-  loader.load('media/test2.json', function(modelGeometry) {
-    var modelMesh = new THREE.Mesh(modelGeometry, modelMaterial);
-    modelMesh.scale.set(40, 40, 40);
-    scene.add(modelMesh);
+  loader.load('media/earth.json', function(geometry) {
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(80, 80, 80);
+    mesh.position.y = -80;
+    scene.add(mesh);
   });
- 
 
-  var planeMaterial = new THREE.MeshStandardMaterial({color: 0xffffff, side: THREE.DoubleSide});
-  var planeGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
-  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  plane.rotation.x = Math.PI / -2;
-  plane.receiveShadow = true;
-  scene.add(plane);
-
-  // scene.fog = new THREE.Fog(0xffffff, 1, 1000); // hex, near, far
-  // scene.fog = new THREE.FogExp2(0xffffff, 0.002); // hex color, density
-
-  renderer = new THREE.WebGLRenderer({alpha: 1, antialias: true});
+  renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
   renderer.setSize(width, height);
-  renderer.shadowMap.enabled = true;
+  controls = new THREE.OrbitControls(myCamera, renderer.domElement);
 
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-  document.body.appendChild(renderer.domElement);
+  container.appendChild(renderer.domElement);
 }
 
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+
+  if (mesh) {
+    mesh.rotation.y += 0.02;
+  }
+
+  renderer.render(scene, myCamera);
   controls.update();
+}
+
+function windowResize() {
+  myCamera.aspect = (window.innerWidth / window.innerHeight);
+  myCamera.updateProjectionMatrix(); // updates camera
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 init();
 animate();
+
+window.addEventListener('resize', windowResize);
