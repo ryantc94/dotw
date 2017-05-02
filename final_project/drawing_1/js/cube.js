43db832e78a7db5cb9972c1f882ca94fa62a3556
ryantc94 <rtc270@nyu.edu>
2017-05-02 05:07:49 -0400

    Drawing1

--- final_project/drawing_1/js/cube.js
+++ final_project/drawing_1/js/cube.js
@@ -1,8 +1,9 @@
 
 var cube = document.getElementsByClassName("cube");
 
-cube.addEventListener("onclick", function() {
+cube.addEventListener("onclick", move());
+
+function move() {
 	var x = event.clientX;     
-	
 	cube.style.transform = "rotateX(" + x + "deg)";
-});
+};
