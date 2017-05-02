94144eb23f26ba36411b0120bd2514390941859f
ryantc94 <rtc270@nyu.edu>
2017-05-02 10:49:58 -0400

    Drawing1

--- final_project/drawing_1/js/cube.js
+++ final_project/drawing_1/js/cube.js
@@ -1,9 +1,41 @@
+// Cube rotation
 
-var cube = document.getElementsByClassName("cube");
+var frontBtn = document.getElementById('front');
+var backBtn = document.getElementById('back');
+var leftBtn = document.getElementById('left');
+var rightBtn = document.getElementById('right');
+var topBtn = document.getElementById('top');
+var bottomBtn = document.getElementById('bottom');
 
-cube.addEventListener("onclick", move());
+var cube = document.getElementById('cube');
 
-function move() {
-	var x = event.clientX;     
-	cube.style.transform = "rotateX(" + x + "deg)";
-};
+function rotateFront() {
+  cube.className = 'rotate-front';
+}
+
+function rotateBack() {
+  cube.className = 'rotate-back';
+}
+
+function rotateLeft() {
+  cube.className = 'rotate-left';
+}
+
+function rotateRight() {
+  cube.className = 'rotate-right';
+}
+
+function rotateTop() {
+  cube.className = 'rotate-top';
+}
+
+function rotateBottom() {
+  cube.className = 'rotate-bottom';
+}
+
+frontBtn.addEventListener('click', rotateFront);
+backBtn.addEventListener('click', rotateBack);
+leftBtn.addEventListener('click', rotateLeft);
+rightBtn.addEventListener('click', rotateRight);
+topBtn.addEventListener('click', rotateTop);
+bottomBtn.addEventListener('click', rotateBottom);
